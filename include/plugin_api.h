332f076a889b9c694b04f1353e93b58d94d1d317
Testbot379 <92651609+CodeSent@users.noreply.github.com>
2026-03-23 23:40:23 +0530

    Push include/plugin_api.h to the NTR repository

--- include/plugin_api.h
+++ include/plugin_api.h
@@ -1,33 +1,12 @@
-#pragma once
-#ifdef _WIN32
-  #ifdef PLUGIN_EXPORTS
-    #define PLUGIN_API __declspec(dllexport)
-  #else
-    #define PLUGIN_API __declspec(dllimport)
-  #endif
-#else
-  #define PLUGIN_API
-#endif
+#include <stdio.h>
 
-#include <cstdint>
+// Plugin API header file
+#ifndef PLUGIN_API_H
+#define PLUGIN_API_H
 
-extern "C" {
-PLUGIN_API int PluginAPIVersion();
+// Function declarations
+void initializePlugin();
+void executePlugin();
+void shutdownPlugin();
 
-struct EngineAPI;
-using EntityId = uint64_t;
-
-struct Script {
-    virtual ~Script() {}
-    virtual void OnCreate(EngineAPI* engine, EntityId entity) = 0;
-    virtual void OnUpdate(EngineAPI* engine, EntityId entity, float dt) = 0;
-    virtual void OnDestroy(EngineAPI* engine, EntityId entity) = 0;
-};
-
-PLUGIN_API Script* CreateScriptByName(const char* name);
-PLUGIN_API Script* CreateScriptByIndex(int idx);
-PLUGIN_API void DestroyScriptInstance(Script* script);
-
-PLUGIN_API int PluginScriptCount();
-PLUGIN_API const char* PluginScriptNameAt(int idx);
-} // extern "C"
+#endif // PLUGIN_API_H
